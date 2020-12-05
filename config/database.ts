f0dd68d54869466114f58476a68f236aed8335aa
Guilhermo Masid <guilhermomasid@gmail.com>
2020-12-05 19:40:23 -0300

    i adding configs to deploy

--- config/database.ts
+++ config/database.ts
@@ -2,9 +2,6 @@ import Env from '@ioc:Adonis/Core/Env'
 import { OrmConfig } from '@ioc:Adonis/Lucid/Orm'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
-import Url from 'url-parse'
-const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
-
 const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
   connection: Env.get('DB_CONNECTION'),
 
@@ -12,11 +9,11 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('PG_HOST', DATABASE_URL.hostname),
-        port: Env.get('PG_PORT', DATABASE_URL.port),
-        user: Env.get('PG_USER', DATABASE_URL.username),
-        password: Env.get('PG_PASSWORD', DATABASE_URL.password),
-        database: Env.get('PG_DB_NAME', DATABASE_URL.pathname.substr(1)),
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
       },
       healthCheck: false,
       debug: false,
